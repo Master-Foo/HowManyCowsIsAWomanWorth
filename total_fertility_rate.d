9caa230e64629edb7b1e9f7c343cfc37550899c7
Master-Foo <Master.Foo.V.1.0.0@Gmail.com>
2019-05-05 19:43:12 -0600

    replace age with age_range

--- total_fertility_rate.d
+++ total_fertility_rate.d
@@ -4,11 +4,11 @@ import std.format;
 import std.stdio;
 
 struct TotalFertilityRate{
-    float age = 0;
+    float[2] age_range = [0,0];
     float fertility_rate = 0;
 
     bool is_age(float age){
-        if(age >= this.age && age < this.age+5){
+        if(age >= this.age_range[0] && age < this.age_range[1]){
            return true;
         }else{
             return false;
@@ -16,21 +16,21 @@ struct TotalFertilityRate{
     }
 
     string toString(){
-        return format("%f %f", this.age, this.fertility_rate);
+        return format("%f - %f: %f", this.age_range[0], this.age_range[1], this.fertility_rate);
     }
 }
 
 TotalFertilityRate[] tfr_graph = [
     //https://www.singstat.gov.sg/modules/infographics/total-fertility-rate
-    {10, 0},
-    {15, 0.026},
-    {20, 0.145},
-    {25, 0.608},
-    {30, 0.930},
-    {35, 0.484},
-    {40, 0.088},
-    {45, 0.005},
-    {49, 0}
+    {[0,  14], 0},
+    {[15, 19], 0.026},
+    {[20, 24], 0.145},
+    {[25, 29], 0.608},
+    {[30, 34], 0.930},
+    {[35, 39], 0.484},
+    {[40, 44], 0.088},
+    {[45, 48], 0.005},
+    {[49, 1000], 0}
 ];
 
 float get_fertility_rate(float age){
